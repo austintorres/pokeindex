390c473bc90164123dbdea589796929cb7bcadbc
austintorres <torresgaustin@gmail.com>
2020-10-04 18:16:59 -0400

    Game decision function added

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -16,9 +16,17 @@ class Pokedex extends Component {
     ]
   };
   render() {
+    let title;
+    if(this.props.isWinner){
+      title = <h1 className="Pokedex-winner">Winning Hand!</h1>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand!</h1>;
+    }
     return (
       <div className="Pokedex">
-        <h1>Pokedex!</h1>
+        <h1>Pokédex!</h1>
+        <p>Total Experience: {this.props.exp}</p>
+        {title}
         <div className="Pokedex-cards">
           {this.props.pokemon.map((p) => (
             <PokeCard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
