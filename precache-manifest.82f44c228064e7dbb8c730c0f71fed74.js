84debdf4f20761eb75fbb2c607980fc906081c01
austintorres <torresgaustin@gmail.com>
2020-10-05 11:53:25 -0400

    Updates

---
+++ precache-manifest.82f44c228064e7dbb8c730c0f71fed74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63acfff7a72c4775a70b63d8294fad79",
+    "url": "/pokeindex/index.html"
+  },
+  {
+    "revision": "f9073ebd6bbb41728f49",
+    "url": "/pokeindex/static/css/main.f30be134.chunk.css"
+  },
+  {
+    "revision": "c0978450cffd4730eed8",
+    "url": "/pokeindex/static/js/2.314e901c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokeindex/static/js/2.314e901c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9073ebd6bbb41728f49",
+    "url": "/pokeindex/static/js/main.2b4059cb.chunk.js"
+  },
+  {
+    "revision": "8d6d034c3ca70d9c780c",
+    "url": "/pokeindex/static/js/runtime-main.9f53a946.js"
+  }
+]);
